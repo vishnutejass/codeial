63bcfd01d0b7da0da31b890a5a9ea304e719edaf
vishnutejass <st3986@srmist.edu.in>
2023-01-24 19:11:53 +0530

    project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+
+app.listen(port,function(err){
+
+    if(err){
+        //console.log('error:',err);
+        console.log(`Error in runing the server:  ${err}`);
+    }
+
+    console.log(`server is running on port:${port}`);
+});
