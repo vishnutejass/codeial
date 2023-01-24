d20388dbf0cb65f1f87e03eacd05108cc5c8acaa
vishnutejass <st3986@srmist.edu.in>
2023-01-24 20:34:06 +0530

    add a controller and router for home

---
+++ controllers/home_controllers.js
@@ -0,0 +1,6 @@
+module.exports.home = function(req,res){
+    return res.end(' document.body.style.backgroundImage="url('d1vzdswwroofzl.cloudfront.net/wp-content/uploads/2023/01/Oscars-2023-Naatu-Naatu-Nominated-For-Best-Original-Song-1.jpg')')
+   
+    return res.end('<h1>Express is up for codeial')
+}
+
--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-
+//use express router
+app.use('/', require('./routes'))
 
 
 app.listen(port,function(err){
 
     if(err){
-        //console.log('error:',err);
+        //console.log('error:',err);   for interpolation use like shown in below
         console.log(`Error in runing the server:  ${err}`);
     }
 
